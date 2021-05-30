d42f64d002cb5699e608aaf1a66a4e8ed53374af
Carlos Daniel <dev.cdfaria@gmail.com>
2021-05-30 16:29:22 -0300

    header scroll animation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,21 @@
 const DOM = {
   body: document.querySelector('body'),
+  header: document.querySelector('#header'),
+  main: document.querySelector('main'),
 
   bodyPreLoad() {
     DOM.body.classList.contains('is-preload') ? DOM.body.classList.remove('is-preload') : ''
+  },
+
+  scrollTrigger() {
+    const mainTriggerTop = DOM.main.getBoundingClientRect().top
+
+    DOM.header.offsetHeight > mainTriggerTop ? DOM.header.classList.add('active') : DOM.header.classList.remove('active')
   }
 }
 
-window.onload = function() {
+window.addEventListener('load', () => {
   DOM.bodyPreLoad()
-}
+  DOM.scrollTrigger()
+})
+window.addEventListener('scroll', DOM.scrollTrigger)
